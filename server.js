8e49d3117336cf9001cd3f322006de0b6769532b
usamasultan2018 <usama.khan2018@gmail.com>
2024-04-09 01:21:34 +0500

    erer

--- server.js
+++ server.js
@@ -3,24 +3,29 @@ const app = express();
 const db = require("./db");
 require('dotenv').config();
 const bodyParser = require('body-parser');
-app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 const MenuItem = require('./models/menu');
-//Get method used to fetch the data from web.
-app.get("/",function(req,res){
-res.send("Welcome to my Hotel");
-});
-
-//Importing the router file
 const personRoutes = require('./routes/personRoute');
 const menuItemRoutes = require('./routes/menuItemRoutes');
 
+// Middleware
+app.use(bodyParser.json());
 
-//Using the routers
-app.use('/person',personRoutes);
-app.use('/menu',menuItemRoutes);
+// Routes
+app.get("/", (req, res) => {
+    res.send("Welcome to my Hotel");
+});
 
+app.use('/person', personRoutes);
+app.use('/menu', menuItemRoutes);
 
-app.listen(PORT,()=>{
-    console.log("Server is running");
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
